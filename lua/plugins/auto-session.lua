aa430351ca5a662875649c6edf119c2682e572d9
Aryan <aryan182282@gmail.com>
2024-04-27 23:03:47 +0530

    blah blah

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,19 @@
+return {
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      auto_restore_enabled = false,
+      log_level = "error",
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      pre_save_cmds = { "Neotree close" },
+    })
+
+    -- vim.keymap.set("n", "<leader>sd", "<cmd>:Autosession delete<cr>", { desc = "Delete Session" })
+    -- vim.keymap.set(
+    -- 	"n",
+    -- 	"<leader>sf",
+    -- 	require("auto-session.session-lens").search_session,
+    -- 	{ desc = "Find Session" }
+    -- )
+  end,
+}
